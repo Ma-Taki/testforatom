75c4089e1556033fa65e0089c9231186d483fe20
SD-PC019 <sd-pc019@SD-PC019-no-MacBook-Pro.local>
2016-06-20 18:26:57 +0900

    サイドバー再び修正

--- resources/views/admin/common/sidebar.blade.php
+++ resources/views/admin/common/sidebar.blade.php
@@ -7,7 +7,8 @@ use App\Libraries\UserUtility;
     <div class="sidebar content-box" style="display: block;">
         <ul class="nav">
             <li class="current"><a href="/admin/top"><i class="glyphicon glyphicon-home"></i> メイン</a></li>
-@if(UserUtility::isDisplaySubMenu(UserUtility::AUTH_TYPE_ITEM_READ))
+@if(UserUtility::isDisplaySubMenu(UserUtility::AUTH_TYPE_ITEM_READ)
+    || UserUtility::isDisplaySubMenu(UserUtility::AUTH_TYPE_MASTER))
             <li class="submenu">
                 <a href="#">
                     <i class="glyphicon glyphicon-briefcase"></i> 案件管理
@@ -19,13 +20,15 @@ use App\Libraries\UserUtility;
                 </ul>
             </li>
 @endif
-@if(UserUtility::isDisplaySubMenu(UserUtility::AUTH_TYPE_MEMBER_READ))
+@if(UserUtility::isDisplaySubMenu(UserUtility::AUTH_TYPE_MEMBER_READ)
+    || UserUtility::isDisplaySubMenu(UserUtility::AUTH_TYPE_MASTER))
             <li><a href="/admin/member/list"><i class="glyphicon glyphicon-user"></i> 会員管理</a></li>
 @endif
-@if(UserUtility::isDisplaySubMenu(UserUtility::AUTH_TYPE_ENTRY_READ))
+@if(UserUtility::isDisplaySubMenu(UserUtility::AUTH_TYPE_ENTRY_READ)
+    || UserUtility::isDisplaySubMenu(UserUtility::AUTH_TYPE_MASTER))
             <li><a href="/admin/entry/list"><i class="glyphicon glyphicon-file"></i> エントリー管理</a></li>
 @endif
-@if(session(SessionUtility::SESSION_KEY_MASTER_FLG))
+@if(UserUtility::isDisplaySubMenu(UserUtility::AUTH_TYPE_MASTER))
             <li class="submenu">
                 <a href="#">
                     <i class="glyphicon glyphicon-wrench"></i> ユーザ管理
