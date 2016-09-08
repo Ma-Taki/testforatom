48fa8114d3e9811d65038cca31319802eed18322
SD-PC019 <sd-pc019@SD-PC019-no-MacBook-Pro.local>
2016-09-08 12:51:35 +0900

    jquery.min.jsを相対パスに変更

--- resources/views/front/common/layout.blade.php
+++ resources/views/front/common/layout.blade.php
@@ -6,12 +6,12 @@
         <meta name="keywords" content="IT案件,案件情報,求人,フリーランス,フリーエンジニア,個人事業主,エンジニア,Java,PHP">
         <meta name="viewport" content="width=device-width,user-scalable=no,maximum-scale=1" />
         <title>@yield('title')</title>
-        <link rel="canonical" href="http://www.engineer-route.com/">
+        <link rel="canonical" href="{{ url('/') }}">
         <link rel="icon" href="{{ url('/front/favicon.ico') }}">
         <link rel="stylesheet" type="text/css" href="{{ url('/front/css/style.css') }}">
         <link rel="stylesheet" type="text/css" href="{{ url('/front/css/slick.css') }}">
         <link rel="stylesheet" type="text/css" href="{{ url('/front/css/slick-theme.css') }}">
-        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
         <script type="text/javascript" charset="utf-8" src="{{ url('/front/js/slick.min.js') }}"></script>
         <script type="text/javascript" charset="utf-8" src="{{ url('/front/js/jquery.tile.js') }}"></script>
     </head>
