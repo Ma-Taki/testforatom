2c36e7b0a5deb9208f0aead8a87a0bddebdb3de0
SD-PC019 <sd-pc019@SD-PC019-no-MacBook-Pro.local>
2016-10-11 17:39:52 +0900

    管理画面：デバッグ用コード除去

--- resources/views/admin/item_detail.blade.php
+++ resources/views/admin/item_detail.blade.php
@@ -165,19 +165,11 @@
             </thead>
             <tbody>
 @foreach($item->entries as $entry)
-{{ dd([$entry->user,
-    $entry->id,
-    $entry->user->last_name,
-    $entry->user->first_name,
-    $entry->user->last_name_kana,
-    $entry->user->first_name_kana,
-    $entry->entry_date->format('Y年n月j日 G時i分'),
-    $entry->delete_flag]) }}
                 <tr>
-                    <th><a href="/admin/entry/detail?id={{ $entry->id }}">EN{{ $entry->id }}</a></th>
-                    <th>{{ $entry->user->last_name }} {{ $entry->user->first_name }}({{ $entry->user->last_name_kana }} {{ $entry->user->first_name_kana }})</th>
+                    <th><a href="{{ '/admin/entry/detail?id='.$entry->id }}">EN{{ $entry->id }}</a></th>
+                    <th>{{ $entry->user->last_name.' '.$entry->user->first_name.'('.$entry->user->last_name_kana.' '.$entry->user->first_name_kana.')' }}</th>
                     <th>{{ $entry->entry_date->format('Y年n月j日 G時i分') }}</th>
-                    <th>{{ $entry->delete_flag ? '無効' : '有効' }}</th>
+                    <th>{{ $entry->delete_flag }}</th>
                 </tr>
 @endforeach
 @endif
