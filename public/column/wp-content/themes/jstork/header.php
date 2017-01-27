309dcde3d39d122eb367dd45cf45391a67d1512a
y.saito@solidseed.co.jp <y.saito@solidseed.co.jp>
2017-01-27 10:22:01 +0900

    wordpress設置

--- public/column/wp-content/themes/jstork/header.php
+++ public/column/wp-content/themes/jstork/header.php
@@ -1,4 +1,10 @@
 <?php
+//require __DIR__.'/../../../../../bootstrap/autoload.php';
+//$app = require_once __DIR__.'/../../../../../bootstrap/app.php';
+//$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+//$response = $kernel->handle(
+//    $request = Illuminate\Http\Request::capture()
+//);
 //require_once '../../../../app/Libraries/FrontUtility.php';
 //require_once 'C:\xampp\htdocs\Engineer-Route\app\Libraries/FrontUtility.php';
 //require_once 'C:\xampp\htdocs\Engineer-Route\vendor\laravel\framework\src\Illuminate\Foundation/helpers.php';
@@ -7,7 +13,7 @@
 //require_once 'C:\xampp\htdocs\Engineer-Route\app\Models/Tr_users.php';
 
 //use App\Libraries\FrontUtility;
-
+//FrontUtility::isLogin();
 //$frontUtility = new FrontUtility();
 
 /*
