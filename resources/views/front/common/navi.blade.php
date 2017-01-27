cf0c0f0aa32a40c2d8aecf634b9851f534436013
y.saito@solidseed.co.jp <y.saito@solidseed.co.jp>
2017-01-27 09:50:00 +0900

    wordpress設置

--- resources/views/front/common/navi.blade.php
+++ resources/views/front/common/navi.blade.php
@@ -3,13 +3,13 @@ use App\Libraries\FrontUtility as FrntUtil;
 ?>
 <nav class="nav">
     <ul class="nav-list">
-        <li class="nav-item"><a href="{{ url('/') }}">HOME</a></li>
+        <li class="nav-item small"><a href="{{ url('/') }}">HOME</a></li>
         <li class="nav-item"><a href="{{ url('/about') }}">エンジニアルートとは</a></li>
         <li class="nav-item"><a href="{{ url('/item/search') }}">新着一覧</a></li>
-        <li class="nav-item"><a href="{{ url('/question') }}">Q&amp;A</a></li>
+        <li class="nav-item small"><a href="{{ url('/question') }}">Q&amp;A</a></li>
         <li class="nav-item"><a href="{{ url('/flow') }}">ご利用の流れ</a></li>
         <li class="nav-item"><a href="{{ url('/company') }}">企業の皆様へ</a></li>
-        <li class="nav-item"><a href="{{ url('/column') }}">コラム</a></li>
+        <li class="nav-item small"><a href="{{ url('/column') }}">コラム</a></li>
 @if(FrntUtil::isLogin())
         <li class="invisible-pc invisible-tab nav-item"><a href="{{ url('/logout') }}">ログアウト</a></li>
 @endif
