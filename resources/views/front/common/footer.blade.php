5bf7a2a87d0ab654734ab07db4d45aa29f47aba1
SD-PC019 <sd-pc019@SD-PC019-no-MacBook-Pro.local>
2017-03-06 12:00:36 +0900

    フッターにfacebookといろんなリンクを追加

--- resources/views/front/common/footer.blade.php
+++ resources/views/front/common/footer.blade.php
@@ -1,9 +1,60 @@
-<footer class="bg clear">
+<div class="clear"></div>
+<footer class="bg">
+  <div id="fb-root"></div>
+  <script>
+  (function(d, s, id) {
+   var js, fjs = d.getElementsByTagName(s)[0];
+   if (d.getElementById(id)) return;
+   js = d.createElement(s); js.id = id;
+   js.src = "//connect.facebook.net/ja_KS/sdk.js#xfbml=1&version=v2.8";
+   fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+  </script>
+
+  <div class="footer-inner invisible-sp">
+
+    <div class="facebook-window">
+      <div class="fb-page"
+           data-href="https://www.facebook.com/engineerroute/?fref=ts"
+           data-small-header="false"
+           data-adapt-container-width="false"
+           data-hide-cover="false"
+           data-show-facepile="true">
+        <blockquote cite="https://www.facebook.com/engineerroute/?fref=ts" class="fb-xfbml-parse-ignore">
+          <a href="https://www.facebook.com/engineerroute/?fref=ts">Engineer-Route</a>
+        </blockquote>
+      </div>
+    </div>
+
+    <div class="sitemap">
+        <h3>オススメの案件・求人</h3>
+        <ul>
+          <li><a href="/item/category/5">Javaの案件・求人</a></li>
+          <li><a href="/item/category/5">PHPの案件・求人</a></li>
+          <li><a href="/item/category/5">AS/400、RPGの案件・求人</a></li>
+          <li><a href="/item/search?job_types%5B%5D=12">WEBデザイナー募集</a></li>
+          <li><a href="/item/search?job_types%5B%5D=12">現場直契約の案件・求人</a></li>
+          <li><a href="/item/search?job_types%5B%5D=12">高単価の案件・求人</a></li>
+        </ul>
+        <h3>お役立ちコラム</h3>
+        <ul>
+          <li><a href="/column/category/c1/">フリーランスの知識</a></li>
+          <li><a href="/column/category/c2/">技術について(開発言語/ツール情報)</a></li>
+          <!--
+          <li><a href="/item/category/5">AS/400、RPGの案件</a></li>
+          <li><a href="/item/search?job_types%5B%5D=12">WEBデザイナー募集</a></li>
+          -->
+        </ul>
+    </div>
+  </div>
+  <div class="footer-links">
     <ul>
-        <li><a href="http://solidseed.co.jp/" target="_blank">運営会社</a></li>
-        <li><a href="{{ url('/privacy') }}">プライバシーポリシー</a></li>
-        <li><a href="{{ url('/terms') }}">利用規約</a></li>
-        <li><a href="{{ url('/contact') }}">お問い合わせ</a></li>
+      <li><a class="hover-thin" href="http://solidseed.co.jp/" target="_blank">運営会社</a></li>
+      <li><a class="hover-thin" href="{{ url('/privacy') }}">プライバシーポリシー</a></li>
+      <li><a class="hover-thin" href="{{ url('/terms') }}">利用規約</a></li>
+      <li><a class="hover-thin" href="{{ url('/contact') }}">お問い合わせ</a></li>
     </ul>
-    <p>&copy; SolidSeed Co.,Ltd.</p>
+  </div>
+  <p>&copy; SolidSeed Co.,Ltd.</p>
+
 </footer>
--- resources/views/front/common/sideInfo.blade.php
+++ resources/views/front/common/sideInfo.blade.php
@@ -1,20 +1,20 @@
 <?php use App\Libraries\FrontUtility as FrntUtil; ?>
 <section class="sideInfo">
-    <div class="invisible-sp">
-        <ul>
+  <div class="invisible-sp">
+    <ul>
 @if(FrntUtil::isLogin() && Request::path() != '/')
-            <li>
-                <div class="hello_user bg">
-                    <p class="wsnw">こんにちは、<br /><a href="{{ url('/user') }}">{{ FrntUtil::getLoginUserName() }}さん</a></p>
-                </div>
-            </li>
+      <li>
+        <div class="hello_user bg">
+          <p class="wsnw">こんにちは、<br /><a href="{{ url('/user') }}">{{ FrntUtil::getLoginUserName() }}さん</a></p>
+        </div>
+      </li>
 @elseif(FrntUtil::isLogin() && Request::path() == '/')
 @else
-            <li>
-                <a href="{{ url('/user/regist/auth') }}">
-                    <img src="/front/images/sbnrSignup.png" alt="無料会員登録。案件紹介をご希望の方はこちら">
-                </a>
-            </li>
+      <li>
+        <a href="{{ url('/user/regist/auth') }}">
+          <img src="/front/images/sbnrSignup.png" alt="無料会員登録。案件紹介をご希望の方はこちら">
+        </a>
+      </li>
 @endif
 <!--
             <li>
@@ -23,37 +23,38 @@
                 </a>
             </li>
 -->
-            <li>
-                <a href="{{ url('/question#aboutFreeEngineer') }}">
-                    <img src="/front/images/sBnr02.png" alt="フリーエンジニアになるためには？">
-                </a>
-            </li>
-            <li>
-                <a href="{{ url('/flow') }}">
-                    <img src="/front/images/sBnr03.png" alt="ご利用案内">
-                </a>
-            </li>
-            <li>
-                <a href="{{ url('/column') }}">
-                    <img src="/front/images/sBnr05.png" alt="コラム">
-                </a>
-            </li>
-            <li>
-                <a href="{{ url('/question') }}">
-                    <img src="/front/images/sBnr04.png" alt="よくある質問">
-                </a>
-            </li>
-        </ul>
-    </div>
-    <div class="invisible-pc invisible-tab">
-        <ul>
+      <li>
+        <a href="{{ url('/question#aboutFreeEngineer') }}">
+          <img src="/front/images/sBnr02.png" alt="フリーエンジニアになるためには？">
+        </a>
+      </li>
+      <li>
+        <a href="{{ url('/flow') }}">
+          <img src="/front/images/sBnr03.png" alt="ご利用案内">
+        </a>
+      </li>
+      <li>
+        <a href="{{ url('/column') }}">
+          <img src="/front/images/sBnr05.png" alt="コラム">
+        </a>
+      </li>
+      <li>
+        <a href="{{ url('/question') }}">
+          <img src="/front/images/sBnr04.png" alt="よくある質問">
+        </a>
+      </li>
+    </ul>
+  </div>
+
+  <div class="invisible-pc invisible-tab">
+    <ul>
 <!--
             <li><a href="/user/regist"><img src="/front/images/sBnr01.png" alt="Engineer-Route News"></a></li>
 -->
-            <li><a href="/question#aboutFreeEngineer"><img src="/front/images/sBnr02.png" alt="フリーエンジニアになるためには？"></a></li>
-            <li><a href="/flow"><img src="/front/images/sBnr03.png" alt="ご利用案内"></a></li>
-            <li><a href="/column"><img src="/front/images/sBnr05.png" alt="コラム"></a></li>
-            <li><a href="/question"><img src="/front/images/sBnr04.png" alt="よくある質問"></a></li>
-        </ul>
-    </div>
+      <li><a href="/question#aboutFreeEngineer"><img src="/front/images/sBnr02.png" alt="フリーエンジニアになるためには？"></a></li>
+      <li><a href="/flow"><img src="/front/images/sBnr03.png" alt="ご利用案内"></a></li>
+      <li><a href="/column"><img src="/front/images/sBnr05.png" alt="コラム"></a></li>
+      <li><a href="/question"><img src="/front/images/sBnr04.png" alt="よくある質問"></a></li>
+    </ul>
+  </div>
 </section><!-- END SIDE-INFO -->
