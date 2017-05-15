6d17db777b37d5503b1f39298d8e0c3a9432296a
井筒大輔 <idutsu@SD-PC020-no-MacBook-Pro.local>
2017-05-15 14:24:01 +0900

    コラムページ検討中ボタン修正

--- public/column/wp-content/themes/jstork/header.php
+++ public/column/wp-content/themes/jstork/header.php
@@ -79,7 +79,7 @@ ga('send', 'pageview');
 
     <div class="user">
       <ul>
-        <li><a href="/considers"><span>検討中</span><span><span id="considers_length"> <?php echo culcConsiderLength() ?></span><span id="considers_unit">件</span></span></a></li>
+        <li><a href="/considers"><span>検討中</span><span><span id="considers_length"> <?php echo culcConsiderLength() ?></span><span id="considers_unit"> 件</span></span></a></li>
       <?php if(frontIsLogin()){ ?>
         <li><a href="/user/entry"><span>応募</span><span>履歴</span></a></li>
         <li><a href="/user"><span>マイ</span><span>ページ</span></a></li>
