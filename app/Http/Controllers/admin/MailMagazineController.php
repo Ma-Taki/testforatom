435d1ec75df9d3a6848af7327c624cf5d0eca4e1
井筒大輔 <idutsu@SD-PC020-no-MacBook-Pro.local>
2017-05-24 17:55:21 +0900

    メルマガ修正

--- app/Http/Controllers/admin/MailMagazineController.php
+++ app/Http/Controllers/admin/MailMagazineController.php
@@ -172,7 +172,7 @@ class MailMagazineController extends Controller
         });
 
         //即時送信の場合は今すぐ送信
-        //if($data_mail['addressFlag'] == AdmnUtil::MAIL_MAGAZINE_SEND_DATE_IMMEDIATELY){
+        if($data_mail['addressFlag'] == AdmnUtil::MAIL_MAGAZINE_SEND_DATE_IMMEDIATELY){
           Mail::send('front.emails.mailmagazine',$data_mail,function ($message) use ($data_mail, $admnUtil) {
             $message->from($admnUtil->mail_magazine_mail_from, $admnUtil->mail_magazine_mail_from_name);
             $message->to($data_mail['toAddressArray']);
@@ -184,7 +184,7 @@ class MailMagazineController extends Controller
               $message->cc($data_mail['bccAddressArray']);
             }
           });
-        //}
+        }
 
     }
 
