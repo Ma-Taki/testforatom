536e2dfc69cf56fcb3868e7344e190b7a838dc76
井筒大輔 <idutsu@SD-PC020-no-MacBook-Pro.local>
2017-05-26 10:03:09 +0900

    メルマガ修正

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,6 +24,6 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->call('emails:send --force')->everyTenMinutes();
+        $schedule->command('emails:send')->everyMinutes();
     }
 }
