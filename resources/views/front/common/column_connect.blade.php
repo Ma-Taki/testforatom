95c5dc2ff29f2d839bf9f1c0190b8fbc17d219c9
hayashi-solid <hayashi.solid@gmail.com>
2018-10-17 11:42:48 +0900

    続きを読む追加

--- resources/views/front/common/column_connect.blade.php
+++ resources/views/front/common/column_connect.blade.php
@@ -4,21 +4,19 @@
         <div class="main-content__body">
             <div class="content__element_bottomSpace">
                 <div class="item">
-                    <a href={{Request::root().'/column/id'.$columnConnect->connect_id.'/'}} target="_blank">
-                        <div class="itemHeader">
-                            <div class="table-row">
-                                <p class="name background_color01 border_color01">
-                                  {{ File::get('./../storage/app/public/id'.$columnConnect->connect_id.'_title.php') }}
-                                </p>
-                                <p class="item_id background_color01 border_color01"><!-- 案件詳細と同じレイアウトにするため空タグ --></p>
-                            </div>
+                    <div class="itemHeader">
+                        <div class="table-row">
+                            <p class="name background_color01 border_color01">
+                              {{ File::get('./../storage/app/public/id'.$columnConnect->connect_id.'_title.php') }}
+                            </p>
+                            <p class="item_id background_color01 border_color01"><!-- 案件詳細と同じレイアウトにするため空タグ --></p>
                         </div>
-                        <div class="itemInfo clear border_color01">
-                            <div class="itemInfoInr">
-                                {!! File::get('./../storage/app/public/id'.$columnConnect->connect_id.'_content.php') !!}
-                            </div>
+                    </div>
+                    <div class="itemInfo clear border_color01">
+                        <div class="itemInfoInr">
+                            {!! File::get('./../storage/app/public/id'.$columnConnect->connect_id.'_content.php') !!}<a href={{Request::root().'/column/id'.$columnConnect->connect_id.'/'}} target="_blank" style="color:#5e8796">続きを読む</a>
                         </div>
-                    </a>
+                    </div>
                 </div>
             </div>
         </div>
