bc6b098ef22c4325b200b34bb38650909752a215
hayashi-solid <hayashi.solid@gmail.com>
2019-02-01 15:02:38 +0900

    userheatタグ

--- resources/views/front/common/layout.blade.php
+++ resources/views/front/common/layout.blade.php
@@ -25,6 +25,11 @@
     @include('front.common.inside_head_tag')
   </head>
   <body>
+    <!-- User Heat Tag -->
+    <script type="text/javascript">
+    (function(add, cla){window['UserHeatTag']=cla;window[cla]=window[cla]||function(){(window[cla].q=window[cla].q||[]).push(arguments)},window[cla].l=1*new Date();var ul=document.createElement('script');var tag = document.getElementsByTagName('script')[0];ul.async=1;ul.src=add;tag.parentNode.insertBefore(ul,tag);})('//uh.nakanohito.jp/uhj2/uh.js', '_uhtracker');_uhtracker({id:'uhqZ5dOcnl'});
+    </script>
+    <!-- End User Heat Tag -->
     <script>
       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
           (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
